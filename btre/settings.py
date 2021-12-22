a9dadc3800500f94dac3243951f6072cfb9bbb47
Sadik Sarac <saracsad@gmail.com>
2021-12-22 11:09:39 +0100

    Removed email from settings

--- btre/settings.py
+++ btre/settings.py
@@ -147,13 +147,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'brad@gmail.com'
-EMAIL_HOST_PASSWORD= ''
-EMAIL_USE_TLS = True
-
 try:
     from .local_settings import *
 except ImportError:
